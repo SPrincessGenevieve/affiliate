9c8ccff1c75793160be2df3de843a9bbc1e315c9
sprincessgenevieve <sprincessgenevieve@gmail.com>
2025-06-13 19:38:46 +0800

    [Update] - updated tier card

--- app/components/dashboard/TierTwo.tsx
+++ app/components/dashboard/TierTwo.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Progress } from "@/components/ui/progress";
 import React from "react";
-import "@/app/globals.css"
+import "@/app/globals.css";
 
 export default function TierTwo() {
   return (
@@ -11,21 +11,31 @@ export default function TierTwo() {
         <Label className="text-[16px]">Tier Progress</Label>
         <div className="w-full h-auto relative flex justify-center items-center">
           <Progress value={40} className="w-full   absolute z-10"></Progress>
-          <div className="w-full flex justify-between z-10">
-            <div className="bg-[#10B981] h-10 w-10 rounded-full flex items-center justify-center">
-              <Label className="text-white text-xl">1</Label>
+          <div className="w-full flex justify-between z-10 ">
+            <div className="bg-gradient-to-r from-[#6C9EA0] to-[#165558] h-10 w-10 rounded-full flex items-center justify-center group hover:text-white">
+              <Label className="group-hover:text-white text-white text-xl">
+                1
+              </Label>
             </div>
-            <div className="bg-[#2E5257] h-10 w-10 rounded-full flex items-center justify-center">
-              <Label className="text-white text-xl">2</Label>
+            <div className="bg-gradient-to-r from-[#BE382B] to-[#901D23] h-10 w-10 rounded-full flex items-center justify-center group hover:text-white">
+              <Label className="group-hover:text-white text-white text-xl">
+                2
+              </Label>
             </div>
-            <div className="bg-[#D1D1D1] h-10 w-10 rounded-full flex items-center justify-center">
-              <Label className="text-gray-500 text-xl">3</Label>
+            <div className="hover:bg-gradient-to-r hover:from-[#CE919B] hover:to-[#C25ABD] bg-[#D1D1D1] h-10 w-10 rounded-full flex items-center justify-center group hover:text-white">
+              <Label className="group-hover:text-white text-gray-500 text-xl">
+                3
+              </Label>
             </div>
-            <div className="bg-[#D1D1D1] h-10 w-10 rounded-full flex items-center justify-center">
-              <Label className="text-gray-500 text-xl">4</Label>
+            <div className="hover:bg-gradient-to-r hover:from-[#DDC4A9] hover:to-[#B7A088] bg-[#D1D1D1] h-10 w-10 rounded-full flex items-center justify-center group hover:text-white">
+              <Label className="group-hover:text-white text-gray-500 text-xl">
+                4
+              </Label>
             </div>
-            <div className="bg-[#D1D1D1] h-10 w-10 rounded-full flex items-center justify-center">
-              <Label className="text-gray-500 text-xl">5</Label>
+            <div className="hover:bg-gradient-to-r hover:from-[#313435] hover:to-[#2A2C2D] bg-[#D1D1D1] h-10 w-10 rounded-full flex items-center justify-center group hover:text-white">
+              <Label className="group-hover:text-white text-gray-500 text-xl">
+                5
+              </Label>
             </div>
           </div>
         </div>

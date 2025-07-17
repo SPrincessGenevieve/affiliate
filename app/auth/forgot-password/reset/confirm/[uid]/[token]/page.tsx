3cded38de134d3addb18c3af43ca113b7c0c17d0
sprincessgenevieve <sprincessgenevieve@gmail.com>
2025-07-17 17:38:50 +0800

    [Fix] - fixed error for unused components

--- app/auth/forgot-password/reset/confirm/[uid]/[token]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ResetPassword() {
-  return (
-    <div>
-      <p>Reset Password</p>
-    </div>
-  );
-}
--- app/components/settings/Billing.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Billing() {
-  return (
-    <div className="w-full h-full flex items-center justify-center">
-      <p>Coming soon...</p>
-    </div>
-  );
-}
